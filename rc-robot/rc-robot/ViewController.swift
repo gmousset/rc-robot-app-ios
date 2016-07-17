6aef6b32e9710dcabce27082b2cb8139a22d338f
gmousset <gwendalmousset@MacBook.local>
2016-07-17 22:35:41 +0200

    Init project

---
+++ rc-robot/rc-robot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rc-robot
+//
+//  Created by Gwendal Mousset on 17/07/2016.
+//  Copyright © 2016 com.github.gmousset. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
